408b1d1a0f2f12bc92db0421ec3ea275a3db13d2
Johan Liesén <johan@liesen.se>
2011-09-10 12:44:12 +0200

    My solution

--- bestbefore.py
+++ bestbefore.py
@@ -1,63 +1,27 @@
-from sys import argv, exit
 from itertools import permutations
 from datetime import date
 
-def illegal(error):
-    #Error processing params
-    print '%s is illegal' % error 
-    exit()
+d = raw_input()
 
-class bestbefore(object):
-    def __init__(self, A, B, C):
-        self.MIN = 0
-        self.MAX = 2999
-        self.YEAR = 2000
-    
-        self.A, self.B, self.C = (int(A), int(B), int(C))
-        # Check input range
-        if not ((self.MIN <= self.A and self.A <= self.MAX) and \
-                (self.MIN <= self.B and self.B <= self.MAX) and \
-                (self.MIN <= self.C and self.C <= self.MAX)):
-            self.A = None
-            self.B = None
-            self.C = None
+try:
+  a, b, c = map(int, d.split('/'))
+except:
+  print "%s is illegal" % d
+  exit()
 
-    def compute(self):
-        # Test all possible options
-        correct = []
-        for A, B, C in permutations([self.A, self.B, self.C]):
-            try:
-                d = date(A, B, C)
-                # Check year in MIN..MAX range
-                if d.year < self.YEAR:
-                    year = d.year + self.YEAR
-                    if year > self.MAX:
-                        # year not in MIN..MAX range!
-                        continue
-                    d = date(year, d.month, d.day)
-                correct.append(d.isoformat())
-            except ValueError:
-                continue
-        if not correct:
-            illegal('/'.join([A, B, C]))
-        # Get earliest legal date
-        return min(correct)
+def trydate(year, month, day):
+  if year < 1000:
+    year += 2000
 
-if __name__ == "__main__":
-    A, B, C = [None, None, None]
-    # Read from a file
-    try:
-        A, B, C = open(argv[1]).read().strip().split('/')
-    except IOError:
-        # If not found, then parse it as string
-        try:
-            A, B, C = argv[1].strip().split('/')
-        except:
-            illegal(argv[1])
+  try:
+    return date(year, month, day)
+  except:
+    return None
 
-    try:
-        b = bestbefore(A, B, C)
-        print b.compute()
-    except:
-        illegal('/'.join([A, B, C]))
+k = sorted(filter(lambda x: x is not None,
+                  map(lambda ks: trydate(*ks), permutations([a, b, c]))))
 
+if len(k) > 0:
+  print k[0]
+else:
+  print "%s is illegal" % d
